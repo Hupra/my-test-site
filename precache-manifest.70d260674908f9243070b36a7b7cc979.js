e6e8a6268da1a07fc4c4355d543636d782c67f2d
unknown <daniel@hupra.net>
2019-01-09 19:56:46 +0100

    Updates

---
+++ precache-manifest.70d260674908f9243070b36a7b7cc979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15e10fff8347f294973e",
+    "url": "/static/js/main.15e10fff.chunk.js"
+  },
+  {
+    "revision": "3795cb15d94e901954ed",
+    "url": "/static/js/1.3795cb15.chunk.js"
+  },
+  {
+    "revision": "15e10fff8347f294973e",
+    "url": "/static/css/main.16bfd36a.chunk.css"
+  },
+  {
+    "revision": "3795cb15d94e901954ed",
+    "url": "/static/css/1.3fb6a4b0.chunk.css"
+  },
+  {
+    "revision": "56cb9584f4163dd63e95349fadbfa523",
+    "url": "/index.html"
+  }
+];
