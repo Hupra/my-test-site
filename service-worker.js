4e55dee61fce1258ff02c1ef01e311570285ac17
unknown <daniel@hupra.net>
2019-01-10 23:08:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f415a59ae21d81c5793bfed2758151bc.js"
+  "/precache-manifest.5759dca3a356bdeda00e1937a6256dd8.js"
 );
 
 workbox.clientsClaim();
