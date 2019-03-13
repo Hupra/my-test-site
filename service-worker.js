dd6f4d5ffffd8c1d0e1cd277bc8d03fd10d78d49
Hupra <36619416+Hupra@users.noreply.github.com>
2019-03-13 11:08:29 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.535afd156ee5ee8801a72be077cf98f5.js"
+  "/precache-manifest.56aea698615b72e1439a15c80a565ae6.js"
 );
 
 workbox.clientsClaim();
